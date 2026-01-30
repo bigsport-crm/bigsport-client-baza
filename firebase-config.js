2d9de0fc34273f6ad8056dc2d86d20cfbc98f697
bigsport-crm <161231201+bigsport-crm@users.noreply.github.com>
2026-01-30 23:17:53 +0600

    Update Firebase configuration and initialization

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,17 @@
 // Firebase Configuration
-// МУҲИМ: Ўзингизнинг Firebase конфигурацияси билан алмаштиринг!
-// Firebase Console дан олган маълумотларингизни бу ерга киритинг
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js';
 const firebaseConfig = {
-    apiKey: "AIzaSyBv8lPj5cbaqcqtzDI-1pw9Wrw24JCvRr1B",
+    apiKey: "AIzaSyDPIrieoenqtwzD2A-QzxHknztZKtOaf18",
     authDomain: "bigsport-crm.firebaseapp.com",
     projectId: "bigsport-crm",
     storageBucket: "bigsport-crm.firebasestorage.app",
-    messagingSenderId: "645659654487",
-    appId: "1:645659654487:web:a63a183926e74db078f3f8"
+    messagingSenderId: "645659654481",
+    appId: "1:645696694481:web:e65a139cb67e68b8787df0",
+    measurementId: "G-RKSKY82609"
 };
+// Initialize Firebase
 let app;
 let auth;
 let db;
